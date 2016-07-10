24b88d7b85250d281ead5eb8d057ec2f66718115
ngn <nick.nickolov@gmail.com>
2016-07-10 08:40:53 +0100

    simplified macro for "for"

--- k.c
+++ k.c
@@ -1,6 +1,6 @@
 #define B break
 #define D default
-#define F(n,b){L _n=(n),i=0;for(;i<_n;i++){b;}}
+#define F(n) for(L _n=(n),i=0;i<_n;i++)
 #define J if
 #define E else
 #define Q case
@@ -34,7 +34,7 @@ S L len(C*x){C*p=x;W(*x)x++;R x-p;}
 S C hex(L x){      R x+(x> 9 ?'a'-10:'0');}
 S L unh(C x){x|=32;R x-(x>'9'?'a'-10:'0');}
 S V ps(C*x){write(2,x,len(x));}
-S V ph(L x){C s[17];s[16]=0;F(16,{s[15-i]=hex(x&15);x>>=4;});write(2,s,17);}
+S V ph(L x){C s[17];s[16]=0;F(16){s[15-i]=hex(x&15);x>>=4;}write(2,s,17);}
 #define pv(x) pv1(#x":",(L)(x))
 S L pv1(C*s,L x){write(2,s,len(s));ph((L)x);write(2,"\n",1);R x;}
 S V pm(V*x,V*y){
@@ -68,11 +68,11 @@ T struct{L z,r,n,t;}*A;
 //memory
 S V*mp0,*mp;
 S V mi(){mp0=mp=(V*)mmap(0,1L<<46,3,0x4022,-1,0);ee("mm",(L)mp<0);}
-S V mc(V*x,V*y,L z){C*p=x,*q=y;F(z,*p++=*q++);}
-S V ms(V*x,C y,L z){C*p=x;F(z,*p++=y);}
+S V mc(V*x,V*y,L z){C*p=x,*q=y;F(z)*p++=*q++;}
+S V ms(V*x,C y,L z){C*p=x;F(z)*p++=y;}
 S L mz(A x){R(max(1,xn)*Z(L));}
 S A ma(C t,L n){A x=mp;xr=1;xt=t;xn=n;mp+=Z(*x)+mz(x);R x;}
-S V mf(A x){J(--xr)R;J(!xt)F(max(1,xn),mf(xA[i]));ms(x,0xaa,Z(*x)+mz(x));}
+S V mf(A x){J(--xr)R;J(!xt)F(max(1,xn))mf(xA[i]);ms(x,0xaa,Z(*x)+mz(x));}
 S A mh(A x){xr++;R x;}
 
 //parser
@@ -86,7 +86,7 @@ S C une(C x){Y(x){Q'0':R'\0';Q'n':R'\n';Q'r':R'\r';Q't':R'\t';D:R x;}}
 S A addL(A x,L y){A z=ma(xt,xn+1);mc(zL,xL,mz(x));zL[xn]=y;mf(x);R z;}
 S A addC(A x,C y){A z=ma(xt,xn+1);mc(zL,xL,mz(x));zC[xn]=y;mf(x);R z;}
 S V ep(L x){J(!x)R;C*p=s,*q=s;W(p>s0&&p[-1]!='\n')p--;W(*q&&*q!='\n')q++;write(2,p,q-p);C b[256];*b='\n';
-            L k=min(s-p,Z(b));F(k,b[i+1]='_');b[k+1]='^';b[k+2]='\n';write(2,b,k+3);ee("parse",1);}
+            L k=min(s-p,Z(b));F(k)b[i+1]='_';b[k+1]='^';b[k+2]='\n';write(2,b,k+3);ee("parse",1);}
 S A prs(C*x){
   s=s0=x;
   J(ltr(*s)){L v=*s++,h=8;W(ldg(*s)){v|=(L)*s++<<h;h+=8;}A x=ma(-11,1);*xL=v;R x;}
@@ -105,7 +105,7 @@ S A eval(A x){
   J(xt==-11){ee("nyi-var",1);R 0;}
   J(xt==11&&xn==1){A z=ma(-11,1);*zL=*xL;R z;}
   J(xt||!xn)R mh(x);
-  A y=ma(0,xn);F(xn,yA[i]=eval(xA[i]));R apply(y);
+  A y=ma(0,xn);F(xn)yA[i]=eval(xA[i]);R apply(y);
 }
 
 //output
@@ -115,9 +115,9 @@ S V oC(C x){J(on==Z(ob))ofl();ob[on++]=x;}
 S V oS(C*x,L n){W(n>Z(ob)-on){mc(ob+on,x,Z(ob)-on);x+=Z(ob)-on;n-=Z(ob)-on;ofl();}J(n){mc(ob+on,x,n);on+=n;}}
 S V oL(L x){C b[32],*u=b+31;I m=x<0;J(m)x=-x;do{*u--='0'+x%10;x/=10;}W(x);J(m)*u--='-';oS(u+1,b+31-u);}
 S V oA(A x){Y(abs(xt)){
-  Q 6:J(xn){F(xn,{J(i)oC(' ');oL(xL[i]);});}E{oS("!0",2);}B;
-  Q 10:{oC('"');F(xn,{C c=esc(xC[i]);J(c){oC('\\');oC(c);}E{oC(xC[i]);}});oC('"');B;}
-  Q 11:F(xn,{oC('`');L v=xL[i];W(v){oC(v&0xff);v>>=8;}});B;
+  Q 6:J(xn){F(xn){J(i)oC(' ');oL(xL[i]);}}E{oS("!0",2);}B;
+  Q 10:{oC('"');F(xn){C c=esc(xC[i]);J(c){oC('\\');oC(c);}E{oC(xC[i]);}}oC('"');B;}
+  Q 11:F(xn){oC('`');L v=xL[i];W(v){oC(v&0xff);v>>=8;}}B;
   D:oS("???",3);B;
 }}
 S V out(A x){oA(x);oC('\n');ofl();}
