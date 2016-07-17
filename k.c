223d83552fadb69313f18d250e1eee9526db62a0
ngn <nick.nickolov@gmail.com>
2016-07-17 18:38:49 +0100

    rearranged source code

--- k.c
+++ k.c
@@ -167,6 +167,33 @@ S A prs(C l){ //parse
   R z;
 }
 
+//output
+S C ob[0x400];S L on=0;                    //buffer
+S V ofl(){J(on){write(1,ob,on);on=0;}}     //flush
+S V oC(C x){J(on==Z(ob))ofl();ob[on++]=x;} //output char
+S V oS(C*x,L n){J(n>Z(ob)-on){ofl();write(1,x,n);}E{mc(ob+on,x,n);on+=n;}} //output string
+S V oL(L x){C b[32],*u=b+31;I m=x<0;J(m)x=-x;do{*u--='0'+x%10;x/=10;}W(x);J(m)*u--='-';oS(u+1,b+31-u);} //output number
+S V oA(A x){ //output array
+  J(xn==1&&0<=xt&&xt<100)oC(',');
+  Y(abs(xt)){
+    Q 0:J(xn==1){oA(*xA);}E{oC('(');F(xn){J(i)oC(';');oA(xA[i]);}oC(')');}B;
+    Q 6:J(xn){F(xn){J(i)oC(' ');oL(xL[i]);}}E{oS("!0",2);}B;
+    Q 10:{I h=0;F(xn)J((xC[i]<32||126<xC[i])&&!esc(xC[i])){h=1;B;}
+          J(h){oS("0x",2);F(xn){oC(hex(xC[i]>>4&15));oC(hex(xC[i]&15));}}
+          E{oC('"');F(xn){C c=esc(xC[i]);J(c){oC('\\');oC(c);}E{oC(xC[i]);}}oC('"');}
+          B;}
+    Q 11:F(xn){oC('`');L v=xL[i];W(v){oC(v&0xff);v>>=8;}}B;
+    Q 99:J(xA[0]->n>1){oA(*xA);oC('!');oA(xA[1]);}E{oC('(');oA(*xA);oS(")!",2);oA(xA[1]);}B;
+    Q 102:oC('{');F(xn){J(i)oC(';');oA(xA[i]);}oC('}');B;
+    Q 103:oA(*xA);oC('[');oA(xA[1]);oC(']');B;
+    Q 104:oA(*xA);oA(xA[1]);B;
+    Q 105:oA(xA[1]);oA(*xA);B;
+    Q 106:Q 107:Q 108:oC(*xC);J(x==cv[*xC][1])oC(':');B;
+    D:oS("???",3);B;
+  }
+}
+S V out(A x){J(x!=cv[':'][1]){oA(x);oC('\n');ofl();}}
+
 //evaluation
 S A pen1(C f,A x){ //penetrate
   J(!xt){A z=ma(0,xn),*p=xA,*r=zA,*r1=r+xn;W(r<r1)*r++=pen1(f,*p++);R z;}
@@ -196,7 +223,6 @@ S A pen2(C f,A x,A y){
   en();R 0;
 }
 S A eval(A,A*,A*);
-S V out(A);
 S A apply(A a,A*l,A*g){
   A f=*a->A;L ft=f->t;
   Y(ft){Q 102:{A d=mh(cd0);F(f->n-1){mf(eval(f->A[i],&d,g));}A z=eval(f->A[f->n-1],&d,g);mf(d);R z;}
@@ -234,33 +260,6 @@ S A eval(A x,A*l,A*g){
     A y=mh(xA[1]);J(yt==-11){A z=eval(xA[2],l,g);*l=dput(*l,*yL,z);R z;}}//assignment
   A y=ma(0,xn);F(xn)yA[i]=eval(xA[i],l,g);R apply(y,l,g);
 }
-
-//output
-S C ob[0x400];S L on=0;                    //buffer
-S V ofl(){J(on){write(1,ob,on);on=0;}}     //flush
-S V oC(C x){J(on==Z(ob))ofl();ob[on++]=x;} //output char
-S V oS(C*x,L n){J(n>Z(ob)-on){ofl();write(1,x,n);}E{mc(ob+on,x,n);on+=n;}} //output string
-S V oL(L x){C b[32],*u=b+31;I m=x<0;J(m)x=-x;do{*u--='0'+x%10;x/=10;}W(x);J(m)*u--='-';oS(u+1,b+31-u);} //output number
-S V oA(A x){ //output array
-  J(xn==1&&0<=xt&&xt<100)oC(',');
-  Y(abs(xt)){
-    Q 0:J(xn==1){oA(*xA);}E{oC('(');F(xn){J(i)oC(';');oA(xA[i]);}oC(')');}B;
-    Q 6:J(xn){F(xn){J(i)oC(' ');oL(xL[i]);}}E{oS("!0",2);}B;
-    Q 10:{I h=0;F(xn)J((xC[i]<32||126<xC[i])&&!esc(xC[i])){h=1;B;}
-          J(h){oS("0x",2);F(xn){oC(hex(xC[i]>>4&15));oC(hex(xC[i]&15));}}
-          E{oC('"');F(xn){C c=esc(xC[i]);J(c){oC('\\');oC(c);}E{oC(xC[i]);}}oC('"');}
-          B;}
-    Q 11:F(xn){oC('`');L v=xL[i];W(v){oC(v&0xff);v>>=8;}}B;
-    Q 99:J(xA[0]->n>1){oA(*xA);oC('!');oA(xA[1]);}E{oC('(');oA(*xA);oS(")!",2);oA(xA[1]);}B;
-    Q 102:oC('{');F(xn){J(i)oC(';');oA(xA[i]);}oC('}');B;
-    Q 103:oA(*xA);oC('[');oA(xA[1]);oC(']');B;
-    Q 104:oA(*xA);oA(xA[1]);B;
-    Q 105:oA(xA[1]);oA(*xA);B;
-    Q 106:Q 107:Q 108:oC(*xC);J(x==cv[*xC][1])oC(':');B;
-    D:oS("???",3);B;
-  }
-}
-S V out(A x){J(x!=cv[':'][1]){oA(x);oC('\n');ofl();}}
 
 //main
 S V exec(C*x,A*l,A*g){
